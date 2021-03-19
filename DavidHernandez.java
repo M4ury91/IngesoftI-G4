693c2d7442a83bedf9c0b1741ccf40b13e702536
cdmorau23 <cdmorau@gmail.com>
2021-03-19 16:59:20 -0500

    CambioaDavid

--- DavidHernandez.java
+++ DavidHernandez.java
@@ -1,5 +1,6 @@
 public class DavidHernandez{
 public static void main(String args []){
-    System.out.println("Hola mundo");
+    System.out.println("mundos Hola");
+    System.out.println("Holi mundo");
 }
 }
