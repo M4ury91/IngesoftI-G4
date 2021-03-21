fbf54f83120dbb8806ac085fa57b22b73eceac2e
kegonzalezs <kegonzalezs@unal.edu.co>
2021-03-21 17:00:30 -0500

    kegonzalezs.java - impresion de alfabeto

--- src/alphabet.java
+++ src/alphabet.java
@@ -2,6 +2,6 @@ public class alphabet {
 
         public static void main(String[] args)
         {
-            System.out.println("¡Hola mundo!");
+            System.out.println("abcdefghijklmnñopqrstuvwxyz");
         }
 }
