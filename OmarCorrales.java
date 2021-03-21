0f8b2d13c316a07ad1b204fe5a0984a636bfdaae
ocorrales <55954686+ocm29@users.noreply.github.com>
2021-03-21 15:41:38 -0500

    prueba

--- OmarCorrales.java
+++ OmarCorrales.java
@@ -1,15 +1,16 @@
   
-public class OmarC {
-
-    public static void main(String[] args) {
-     System.out.print("Mi rama modíficada"); 
-     System.out.print("Mi rama modíficada 2");
-    
-    
-    public static int sumaNumeros(int a, int b){
-
-        return a+b;
+public class OmarCorrales{
+   static  int sumaNumeros(int a, int b){
+           int c = a+b;
+        return c;
     }
+    public static void main(String[] args) {
+     System.out.println("Mi rama modíficada"); //
+
+     System.out.println("Mi rama modíficada 2");
+    //
+    System.out.println(sumaNumeros(14,2));
+  
 } 
     
 
