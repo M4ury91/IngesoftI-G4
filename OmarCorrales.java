400c74e76e9587ed6602c393e6e41e1b7f87d236
ocorrales <55954686+ocm29@users.noreply.github.com>
2021-03-21 15:03:05 -0500

    New

---
+++ OmarCorrales.java
@@ -0,0 +1,12 @@
+  
+public class OmarC {
+
+    public static void main(String[] args) {
+     System.out.print("Mi rama modíficada"); 
+     System.out.print("Mi rama modíficada 2");
+} 
+    
+
+
+
+}
